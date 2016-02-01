81dd517acd20ded29361ef0352e1384fb1106385
Ben Rexin <ben@rexin.at>
2016-02-01 17:40:00 +0100

    feat: skip unrecognizable commits

--- lib/conventional_changelog/git.rb
+++ lib/conventional_changelog/git.rb
@@ -5,8 +5,9 @@ module ConventionalChangelog
     def self.commits(options)
       log(options).split("\n").map { |commit| commit.split DELIMITER }.select { |commit| options[:since_date].nil? or commit[1] > options[:since_date] }.map do |commit|
         comment = commit[2].match(/(\w*)(\(([\w\$\.\-\* ]*)\))?\: (.*)/)
+        next unless comment
         { id: commit[0], date: commit[1], type: comment[1], component: comment[3], change: comment[4] }
-      end
+      end.compact
     end
 
     def self.log(options)
